8230b3da1c3f9f41dcdc04190103b45e040f2c30
landon tipantiza <landontipantiza@gmail.com>
2020-05-26 04:31:14 +0000

    created function that recreates tale of givin array'

---
+++ tale.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🔵🔵🔵Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
