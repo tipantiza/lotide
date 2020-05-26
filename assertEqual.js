e033d0e087ec9501ad470213ec2e558b3382c8c0
landon tipantiza <landontipantiza@gmail.com>
2020-05-26 03:49:17 +0000

    changed concat to template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🔵🔵🔵Assertion Passed: " + actual + " === " + expected);
+    console.log(`🔵🔵🔵Assertion Passed: ${actual} ===  ${expected}`) ;
   } else {
-    console.log("🔴🔴🔴Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
