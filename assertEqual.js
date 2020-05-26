ed6f607d4663a89dfd7982db74dd5897f0cb32eb
landon tipantiza <landontipantiza@gmail.com>
2020-05-26 03:37:13 +0000

    created assert function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🔵🔵🔵Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🔴🔴🔴Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1,2);
+assertEqual("hi", "hi");
