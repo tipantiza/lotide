83427d1366fc0377f73e11bd0330540de6dd1531
landon tipantiza <landontipantiza@gmail.com>
2020-05-27 02:16:35 +0000

    created function that displays onto console whether or not 2 arrays are equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function (arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length){
+    return false
+  }
+  for (var i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] !== arrayB[i]) {
+      return false;
+    } 
+  }
+  return true;
+}
+
+const assertArraysEqual = function (arrayA, arrayB) {
+  if (eqArrays(arrayA, arrayB)){
+    console.log(`🔵🔵🔵Assertion Passed: ${arrayA} ===  ${arrayB}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${arrayA} !== ${arrayB}`);
+  }
+}
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4])
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 3])
