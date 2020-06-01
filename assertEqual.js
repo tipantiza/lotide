9f4651a234bddcb8f65632893c3a5db8214e47ae
landon tipantiza <landontipantiza@gmail.com>
2020-06-01 21:27:48 +0000

    started refactor creating test files for each .js file

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1,2);
-assertEqual("hi", "hi");
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,12 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 const head = function(array) {
   return array[0];
 };
-assertEqual(head([5]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
