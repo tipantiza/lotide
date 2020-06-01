419af6a692f982e1797fd3a35cc23fd34762e5ed
landon tipantiza <landontipantiza@gmail.com>
2020-06-01 23:44:38 +0000

    incorperated mocha and chai into test files

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,11 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert
 const tail = require('../tail')
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+
+describe('#tail', () => {
+  it('returns [2,3,4,5] for [1,2,3,4,5]', () => {
+    assert.deepEqual(tail([1,2,3,4,5]), [2,3,4,5]);
+  });
+  it("returns ['hello', 'how', 'are', 'you'] for ['landon', 'how', 'are', 'you']", () => {
+    assert.deepEqual(tail(['landon', 'how', 'are', 'you']), ['how', 'are', 'you'], "[message]");
+  })
+});
